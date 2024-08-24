616815191a6442de7f9d4fc425e3e7ed5c923482
tanishqawasthi <awasthitanishq03@gmail.com>
2024-08-24 22:19:11 +0530

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#FF6F3C",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
